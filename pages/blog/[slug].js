3d472828c16b3cd0e31368a4b802d14b8805bde7
r3dreck <todd@consumedesign.com>
2021-05-04 16:21:16 -0400

    loading again

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -71,7 +71,7 @@ function Page(props) {
   if (router.isFallback) {
     return (
       <>
-        <div className="loading">Loading...</div>
+        <div className="loading full-screen">Loading...</div>
       </>
     )
   } else {
